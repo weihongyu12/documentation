58787fdcee3e7bbc13bd9cfab4c49ca018101ab1
weihongyu12 <weihongyu12@outlook.com>
2024-09-09 09:53:00 +0800

    增加github action

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,8 +15,8 @@ const config: Config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'weihongyu12', // Usually your GitHub org/user name.
+  projectName: 'weihongyu12.github.io', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
